228e2c71248aaeaa8dd78f72ff4ffc0576c27a97
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2025-10-20 15:19:25 +0200

    1. UI refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,50 @@
 import { Component } from '@angular/core';
 import { NgClass } from '@angular/common';
 
+type TaskStatus = 'Completed' | 'Pending' | 'Planned';
+interface Task {
+  name: string;
+  status: TaskStatus;
+  date: string; // ISO yyyy-mm-dd
+  description: string;
+  expanded: boolean;
+}
+
 @Component({
   selector: 'app-root',
   imports: [NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  standalone: true
+  standalone: true,
 })
 export class AppComponent {
   title = 'junior-frontend-developer-task';
 
-  protected tasks = [
-    { name: 'Zrobić zakupy spożywcze', status: 'Completed', date: '2025-05-01', description: 'Muszę kupić mleko, mąkę i jajka.' },
-    { name: 'Opłacić rachunki', status: 'Pending', date: '2025-05-10', description: 'Tylko nie odkładaj tego na inny dzień!' },
-    { name: 'Urodziny mamy', status: 'Planned', date: '2025-05-15', description: 'Kupić kwiaty i tort.' }
+  protected tasks: Task[] = [
+    {
+      name: 'Zrobić zakupy spożywcze',
+      status: 'Completed',
+      date: '2025-05-01',
+      description: 'Muszę kupić mleko, mąkę i jajka.',
+      expanded: false,
+    },
+    {
+      name: 'Opłacić rachunki',
+      status: 'Pending',
+      date: '2025-05-10',
+      description: 'Rachunek PGNiG na Gmailu 20.10.2025',
+      expanded: false,
+    },
+    {
+      name: 'Urodziny mamy',
+      status: 'Planned',
+      date: '2025-05-15',
+      description: 'Kupić kwiaty i tort.',
+      expanded: false,
+    },
   ];
-  protected descVisible: boolean = false;
 
-  toggleCompleted(task: any) {
+  toggleCompleted(task: Task) {
     task.status = task.status === 'Completed' ? 'Planned' : 'Completed';
-  }
-
-  toggleDescription() {
-    this.descVisible = !this.descVisible;
   }
 }
